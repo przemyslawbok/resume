e24ce3b1e2d04f482fa1b7e8aa340a624238fb67
Jules Caesaris <julescaesaris@gmail.com>
2023-02-13 13:03:59 +0100

    #10: Setup Firebase

--- utils/firebase/firebase.ts
+++ utils/firebase/firebase.ts
@@ -3,15 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-// TODO: setup Firebase for Resume project
 const firebaseConfig = {
-  apiKey: 'AIzaSyAG2SZ1sofMdlD4jPjIZjqGN_hrL0SdEVI',
-  authDomain: 'zeus-todo.firebaseapp.com',
-  projectId: 'zeus-todo',
-  storageBucket: 'zeus-todo.appspot.com',
-  messagingSenderId: '639366391654',
-  appId: '1:639366391654:web:e2065f498789f38dd34aa7',
-  measurementId: 'G-JH817E2S6H',
+  apiKey: 'AIzaSyDKDNjVpy1N7UJG2FuYl3lP4ehoJbRBgNE',
+  authDomain: 'resume-4f517.firebaseapp.com',
+  projectId: 'resume-4f517',
+  storageBucket: 'resume-4f517.appspot.com',
+  messagingSenderId: '345690797519',
+  appId: '1:345690797519:web:7a13f1a4ec38d073dd4eba',
+  measurementId: 'G-Z2MFBGFFYM',
 };
 
 // Initialize Firebase
