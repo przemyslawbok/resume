8fdff54315139a9dda311a3d8b461d6d562aa16b
EugeneBachura <eugenebachura@gmail.com>
2023-02-15 15:16:04 +0100

    #19: Fix index re-export

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,5 @@
 import { UserContext } from 'utils/contexts/UserContext';
-import { UserProfile } from 'components/UserProfile';
+import { UserProfile } from 'components';
 import { useContext } from 'react';
 
 const AdminPage = () => {
