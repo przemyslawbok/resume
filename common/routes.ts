4bb5f34d9ab10d8b243ef5269263ade70ba86695
EugeneBachura <eugenebachura@gmail.com>
2023-02-16 14:47:28 +0100

    #20: Add mantine style
    + add Home router

--- common/routes.ts
+++ common/routes.ts
@@ -1,4 +1,5 @@
 export const Routes = {
+  Home: '/',
   Admin: '/admin',
   UserProfile: '/userProfile',
   Resume: '/resume',
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,14 +2,10 @@ import { NavBar } from 'components';
 import { UserContext } from 'utils/contexts/UserContext';
 import { useContext } from 'react';
 
-const AdminPage = () => {
-  const { email } = useContext(UserContext);
-
-  return (
-    <main>
-      <NavBar />
-    </main>
-  );
-};
+const AdminPage = () => (
+  <main>
+    <NavBar />
+  </main>
+);
 
 export default AdminPage;
