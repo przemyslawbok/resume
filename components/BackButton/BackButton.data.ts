10cb9efed854ef3856310dfcb11ec09373f2d4a2
byalit <92673028+byalit@users.noreply.github.com>
2023-03-02 15:51:48 +0100

    #27: changed BackButton component using useTranslation hook

--- components/BackButton/BackButton.data.ts
+++
@@ -1,3 +0,0 @@
-export interface BackButtonProps {
-  text?: string;
-}
--- components/BackButton/BackButton.tsx
+++ components/BackButton/BackButton.tsx
@@ -1,15 +1,18 @@
-import { BackButtonProps } from './BackButton.data';
 import { Button } from '@mantine/core';
 import { FC } from 'react';
 import { useRouter } from 'next/router';
+import { useTranslation } from 'utils/hooks';
 
-const BackButton: FC<BackButtonProps> = ({ text = 'Back' }) => {
+const BackButton: FC = () => {
   const router = useRouter();
+  const [t] = useTranslation();
 
   const handleBackButtonClick = () => {
     router.back();
   };
 
-  return <Button onClick={handleBackButtonClick}>{text}</Button>;
+  return (
+    <Button onClick={handleBackButtonClick}>{t('buttonLabel.back')}</Button>
+  );
 };
 export default BackButton;
